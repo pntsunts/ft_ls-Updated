38e3e314922c6d423097ee2fe06356cd800ae68b
Peter NTSUNTSHA <pntsunts@c5r2s3.wethinkcode.co.za>
2020-02-10 12:37:18 +0200

    ft_ls.h

---
+++ ft_ls.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pntsunts <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/10 12:31:21 by pntsunts          #+#    #+#             */
+/*   Updated: 2020/02/10 12:36:48 by pntsunts         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+#define FT_LS_H
+
+#include "libft/libft.h"
+#include <dirent.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <errno.h>0
+
+#endif
