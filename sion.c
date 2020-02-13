25dbf2f0b95320bee0a10aad903edfd6fa9534ec
Peter NTSUNTSHA <pntsunts@c5r2s3.wethinkcode.co.za>
2020-02-13 10:22:43 +0200

    recur.c sion.c

--- sion.c
+++ sion.c
@@ -6,8 +6,35 @@
 /*   By: pntsunts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 15:23:18 by pntsunts          #+#    #+#             */
-/*   Updated: 2020/02/12 16:34:04 by pntsunts         ###   ########.fr       */
+/*   Updated: 2020/02/13 10:22:06 by pntsunts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_ls.h"
 
+static	void recursion(const char *dir_name)
+{
+	DIR *directory;
+	char *data;
+	struct dirent *folder;
+
+	directory = opendir(dir_name);
+	data = (char *)malloc(sizeof(char) * 4080);
+
+	if (!directory)
+	{
+		ft_putstr("Can't open the directory");
+		return ;
+	}
+	ft_putchar('\n');
+	while ((folder = readdir(directory)) != NULL)
+	{
+		if (folder->d_name[0] != '.' && folder->d_type == DT_DIR)
+		{
+			ft_strcpy(data, dir_name);
+			ft_strcat(data, "/");
+			ft_strcat(data, folder->d_name);
+			list_dir(data);
+		}
+	}
+}
