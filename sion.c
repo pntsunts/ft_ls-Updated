3528ded20d6709472ebf1945ca410659e06dac27
Peter NTSUNTSHA <pntsunts@c5r2s3.wethinkcode.co.za>
2020-02-27 14:48:42 +0200

    Normed

--- sion.c
+++ sion.c
@@ -6,21 +6,20 @@
 /*   By: pntsunts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/12 15:23:18 by pntsunts          #+#    #+#             */
-/*   Updated: 2020/02/17 11:50:33 by pntsunts         ###   ########.fr       */
+/*   Updated: 2020/02/27 14:48:19 by pntsunts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void sion(const char *dir_name)
+void	sion(const char *dir_name)
 {
-	DIR *directory;
-	char *data;
-	struct dirent *folder;
+	DIR				*directory;
+	char			*data;
+	struct dirent	*folder;
 
 	directory = opendir(dir_name);
 	data = (char *)malloc(sizeof(char) * 4080);
-
 	if (!directory)
 	{
 		ft_putstr("Can't open the directory");
