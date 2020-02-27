49307c863580ddbbc062019b3ebbbdeda8e85f59
Peter NTSUNTSHA <pntsunts@c5r2s3.wethinkcode.co.za>
2020-02-27 14:54:22 +0200

    Normed

--- recur.c
+++ recur.c
@@ -6,29 +6,27 @@
 /*   By: pntsunts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/11 15:03:22 by pntsunts          #+#    #+#             */
-/*   Updated: 2020/02/17 13:55:58 by pntsunts         ###   ########.fr       */
+/*   Updated: 2020/02/27 14:53:02 by pntsunts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void recur(const char *dir_name)
+void	recur(const char *dir_name)
 {
-	DIR *directory;
-	char *data;
-	struct dirent *folder;
+	DIR				*directory;
+	char			*data;
+	struct dirent	*folder;
 
 	directory = opendir(dir_name);
 	ft_putendl(dir_name);
 	data = (char *)malloc(sizeof(char) * 4080);
-
 	if (!directory)
 	{
 		ft_putstr("Cant open the Directory");
 		return ;
 	}
 	while ((folder = readdir(directory)) != NULL)
-	{
 		if (folder->d_name[0] != '.')
 		{
 			ft_putstr(folder->d_name);
@@ -38,7 +36,6 @@ void recur(const char *dir_name)
 			ft_putchar('\t');
 			ft_strcat(data, folder->d_name);
 		}
-	}
 	closedir(directory);
 	free(data);
 }
