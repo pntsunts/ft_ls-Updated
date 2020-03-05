c078949e8fa3b57bd9b47b1dc648c8a83aee9394
Peter NTSUNTSHA <pntsunts@c5r2s3.wethinkcode.co.za>
2020-03-05 13:59:54 +0200

    ft_printl.c

--- ft_printl.c
+++ ft_printl.c
@@ -6,29 +6,34 @@
 /*   By: pntsunts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/02 15:43:59 by pntsunts          #+#    #+#             */
-/*   Updated: 2020/03/02 16:16:56 by pntsunts         ###   ########.fr       */
+/*   Updated: 2020/03/05 13:59:20 by pntsunts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	printing(char **av, t_files data[])
+void	printing(char **av, t_files tmp[])
 {
 	int i = 1;
 	int j = 0;
-	while (ft_checkf(av[i]))
-		i++;
+	char *s1;
+
 	while (av[i])
 	{
-		if (ft_isdir(av[i]) == 0)
-			printl(av[i]);
+		if (ft_isdir(av[i]) && !ft_checkf(av[i]))
+		{
+			loopdir(av[i], tmp);
+			j = 0;
+			while (tmp[j].name != NULL)
+			{
+				s1 = ft_strjoin(ft_strjoin(av[i], "/"), tmp[j].name);
+				printl(s1);
+				j++;
+			}
+		}
 		else
 		{
-			//ft_putstr("this is directory");
-			//"printl(loopdir(av, data);
-			loopdir(av[i], data);
-			while (data[j].name != NULL)
-				printl(av[j++]);
+			printl(av[i]);
 		}
 		i++;
 	}
