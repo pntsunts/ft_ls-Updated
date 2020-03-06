f3233ef78d9ba49ae0546ffa3a54a90ee886e49d
Peter NTSUNTSHA <pntsunts@c5r2s3.wethinkcode.co.za>
2020-03-06 11:04:44 +0200

    updated

--- printr.c
+++ printr.c
@@ -6,7 +6,7 @@
 /*   By: pntsunts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/02 11:21:30 by pntsunts          #+#    #+#             */
-/*   Updated: 2020/03/02 15:48:08 by pntsunts         ###   ########.fr       */
+/*   Updated: 2020/03/06 11:03:29 by pntsunts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,45 @@
 
 void	printr(const char *dir_name, t_files store[])
 {
-//	t_files store[1024];
 	int i = 0;
+	//revsort(store);
 	printrev(dir_name, store);
+	revsort(store);
 	while (store[i].name != NULL)
 		i++;
 	i--;
 	while (i >= 0)
 		ft_putendl(store[i--].name);
 	return ;
+}
+
+void	revsort(t_files data[])
+{
+	int i;
+	int j;
+	char *tmp;
+
+	j = 0;
+	while (!j)
+	{
+		i = 0;
+		while (data[i + 1].name)
+		{
+			if (ft_strcmp(data[i].name, data[i + 1].name) > 0)
+			{
+				tmp = data[i].name;
+				data[i].name = data[i + 1].name;
+				data[i + 1].name  = tmp;
+			}
+			i++;
+		}
+		i = 0;
+		j = 1;
+		while (data[i + 1].name && j)
+		{
+			if (ft_strcmp(data[i].name, data[i + 1].name) > 0)
+				j = 0;
+			i++;
+		}
+	}
 }
