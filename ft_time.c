5adfdd162a866a73212105ee5a9afc962b98db42
Peter NTSUNTSHA <pntsunts@c5r2s3.wethinkcode.co.za>
2020-03-10 14:50:30 +0200

    updated

--- ft_time.c
+++ ft_time.c
@@ -6,7 +6,7 @@
 /*   By: pntsunts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/06 09:43:29 by pntsunts          #+#    #+#             */
-/*   Updated: 2020/03/09 16:36:17 by pntsunts         ###   ########.fr       */
+/*   Updated: 2020/03/10 14:49:19 by pntsunts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,24 @@ void	ft_time(const char *dir_name, t_files data[])
 	struct dirent 	*folders;
 	struct stat   t_f;
 	int i = 0;
+	char path[1042];
 
+	ft_bzero(path, 1);
+	ft_strcat(path, dir_name);
 	directory = opendir(dir_name);
 	while ((folders = readdir(directory)) != NULL)
 	{
 		if (folders->d_name[i] != '.')
 		{
+			ft_strcat(path, folders->d_name);
+			//lstat(path, (data[i].stptr));
+			//lstat(path, &t_f);
 			data[i].name = ft_strdup(folders->d_name);
+			if (lstat(data[i].name, &t_f) < 0)
+			{
+				printf("This sucks");
+				return ;
+			}
 			data[i].mtime = t_f.st_mtime;
 			i++;
 		}
@@ -32,100 +43,62 @@ void	ft_time(const char *dir_name, t_files data[])
 	closedir(directory);
 }
 
-void	printtime(t_files store[])
+void printtime(const char *dir_name, t_files data[])
 {
-//	int i = 0;
-	ft_time(".", store);
-	distime(store,length(store)); 
-
+	ft_time(dir_name, data);
+	ft_displaytime(data, ft_structlen(data));
 }
 
-void	distime(t_files data[], int i)
+int		ft_structlen(t_files length[])
 {
-	i--;
-
-	ft_putendl("OPPPPPPS");
-	sortime(data, i);
-	while (i >= 0)
-	{
-		if (*data[i].name != '.')
-		{
-			ft_putendl(data[i].name);
-			ft_strdel(&data[i].name);
-		}
-		i--;
-	}
-}
-
-int		length(t_files len[])
-{
-	int i;
+	int	i;
 
 	i = 0;
-	while (len[i].name != NULL)
+	while (length[i].name != NULL)
 		i++;
 	return (i);
 }
 
-void		swap(t_files	*data, int i)
+void	ft_displaytime(t_files test[], int n)
 {
-	t_files		tmp;
-
-	if (ft_strcmp(data[i].name , data[i + 1].name) > 0)
+	n--;
+	number_sort(test, n);
+	while (n >= 0)
 	{
-		tmp = data[i];
-		data[i] = data[i + 1];
-		data[i + 1] = tmp;
-	}
-}
-
-void	strset(t_files *data, int i)
-{
-	struct timespec s1;
-	struct timespec s2;
-
-	s1 = data[i].stptr.st_mtimespec;
-	s2 = data[i + 1].stptr.st_mtimespec;
-}
-
-int		timesort(t_files *data, int i)
-{
-	struct timespec s1;
-	struct timespec s2;
-	t_files tmp;
-
-	if (s1.tv_sec < s2.tv_sec)
-	{
-		tmp = data[i];
-		data[i] = data[i + 1];
-		data[i + 1] = tmp;
-		return (-1);
-	}
-	else if (s1.tv_sec == s2.tv_sec)
-		swap(data, i);
-	return (0);
-}
-
-void	sortime(t_files *data, int i)
-{
-	int y;
-	int x;
-	int z;
-//	struct timespec s1;
-//	struct timespec s2;
-
-	z = i > 1;
-	while (z)
-	{
-		x = -1;
-		z = 0;
-		while (++x < (i - 1))
+		if (*test[n].name != '.')
 		{
-			strset(data, i);
-			y = timesort(data, i);
-			z = (y == -1) ? 1 : 0;
-			if (z == 1)
-				break;
+			ft_putendl(test[n].name);
+			ft_strdel(&test[n].name);
 		}
+		n--;
+	}
+}
+
+void	number_sort(t_files order[], int n)
+{
+	int		i;
+	int		j;
+	t_files	temp;
+
+	i = 0;
+	while (i < n - 1)
+	{
+		j = 0;
+		//printf("Index is >> %d, %ld ", i, order[i].mtime);
+		//printf(" %s\n", order[i].name);
+		while (j < n - i - 1)
+		{
+		//	printf("Current: %ld\nNext: %ld\n", order[j].mtime, order[j + 1].mtime);
+			//if (order[j].mtime > order[j + 1].mtime)
+			if (order[j].mtime > order[j + 1].mtime)
+			{
+				ft_putstr("Swapping values");
+				temp = order[j];
+				order[j] = order[j + 1];
+				order[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
 	}
 }
