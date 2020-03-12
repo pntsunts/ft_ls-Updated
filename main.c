9199a46f6c5c67cf60b43babc077839bc0177490
Peter NTSUNTSHA <pntsunts@c5r2s3.wethinkcode.co.za>
2020-03-12 13:53:33 +0200

    updated

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pntsunts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 10:26:22 by pntsunts          #+#    #+#             */
-/*   Updated: 2020/03/06 14:50:01 by pntsunts         ###   ########.fr       */
+/*   Updated: 2020/03/12 13:53:11 by pntsunts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,29 +15,51 @@
 int		main(int ac, char **av)
 {
 	P_flags *F_lags = (P_flags*)malloc(sizeof(P_flags));
-	//t_files *tmp = (t_files*)malloc(sizeof(t_files));
 	t_files tmp[1024];
 	int i = 0;
 	if (ac < 2)
 	{
 		if (av[i][0] != '-')
 		{
-			ft_ls("./");
+			ft_ls(".");
 		}
 	}
 	else
 	{
-		//ft_perm(av[1]);
-		//ft_checkf(ac, av);
-		//ft_confirm(ac, av, &F_lags);
 		test(ac, av, F_lags, tmp);
 	}
 	sleep(10);
 	return (0);
 }
-
-/*int main(void)
+void	test(int ac, char **av, P_flags *F_lags, t_files tmp[])
 {
-	sion("./");
-	recur("/");
-}*/
+	int i;
+	i = 1;
+
+	while (i < ac && ft_checkf(av[i]))
+	{
+		ft_confirm(ac, av, F_lags);
+		if (F_lags->l_flag == 1)
+		{
+			if (ac > 2)
+			{
+				printing(av, tmp);
+			}
+			else
+				see(".", tmp);
+		}
+   		if (F_lags->Re_flag == 1)
+		{
+			sion(".");
+			recur(".");
+		}
+		if (F_lags->a_flag == 1)
+			ft_ls_a(".");
+		if (F_lags->r_flag == 1)
+			printr(".", tmp);
+		if (F_lags->t_flag == 1)
+			printtime(".", tmp);
+		i++;
+	}
+}
+
